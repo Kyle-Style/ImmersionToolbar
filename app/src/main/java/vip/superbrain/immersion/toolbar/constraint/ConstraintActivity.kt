93cfdfbb2a92e0a2b06d10b2e2ba834b9ad22b00
wangleilei <wangleilei@mockuai.com>
2020-02-25 19:48:38 +0800

    constraintlayout

---
+++ app/src/main/java/vip/superbrain/immersion/toolbar/constraint/ConstraintActivity.kt
@@ -0,0 +1,18 @@
+package vip.superbrain.immersion.toolbar.constraint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @Description
+ *
+ * @Author wangleilei
+ * @Email wangleilei@mockuai.com
+ * @Date 2020-02-25 13:11
+ */
+class ConstraintActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
